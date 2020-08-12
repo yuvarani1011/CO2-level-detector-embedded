6d98a392c0ab234216e388d0184285c7222df927
yuvarani1011 <50730762+yuvarani1011@users.noreply.github.com>
2020-08-12 23:07:11 +0530

    Create write.php

---
+++ write.php
@@ -0,0 +1,13 @@
+<?php
+  $val = $_GET["data"];
+  $fileContent=$val."\n";
+  $fileStatus=file_put_contents("datastorage.txt",$fileContent,FILE_APPEND);
+  if($fileStatus == true)
+  {
+    echo "SUCCESS. Data written in file.";
+  }
+  else
+  {
+    echo "FAIL. Could not connect to file.";
+  }
+?>
